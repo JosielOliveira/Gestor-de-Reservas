550a11b81b29515d59d517600f4eda3f291df405
JosielOliveira <joliviera.prof@gmail.com>
2024-12-25 19:36:33 +0100

    changes models/User

--- models/User.js
+++ models/User.js
@@ -1,11 +1,26 @@
+// models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,  // Asegura que el email sea único
+    lowercase: true, // Convierte el email a minúsculas
+  },  
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  }
 });
 
+// Asegúrate de encriptar la contraseña antes de guardar el usuario en la base de datos 
 userSchema.pre('save', async function(next) {
   if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 10);
